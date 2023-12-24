8ee290e6ba5917be20fa8698fea120fd24ec0202
Max <MaksKamarnitskiy@Gmail.com>
2023-12-24 14:21:31 +0300

    Les_1: Task_2 (complete)

--- Les_1/1_2/Program.cs
+++ Les_1/1_2/Program.cs
@@ -3,9 +3,27 @@
 
 Console.Write("Enter a number 1: ");
 int b1 = int.Parse(Console.ReadLine()!);
-int count = -b1;
-while (count <= b1)
+if (b1 > 0)
 {
-    Console.Write(count+" ");
-    count++;
+    int count = -b1;
+    while (count <= b1)
+    {
+        Console.Write(count+" ");
+        count++;
+        
+    }
+}
+else if (b1 < 0)
+{
+    int count = -b1;
+    while (count >= b1)
+    {
+        Console.Write(-count+" ");
+        count--;
+        
+    }
+}
+else
+{
+    Console.Write("Your nomber - ZERO!");
 }
