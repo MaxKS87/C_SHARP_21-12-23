26dea8919e327b827099dabd6774c11b3970fc32
Max <MaksKamarnitskiy@Gmail.com>
2023-12-26 00:36:30 +0300

    Les_2: HomeWork: Task_2

---
+++ Les_2/HomeWork/2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости,
+// в которой находится эта точка.
+
+Console.Write("Введём абсциссу точки (X): ");
+int x = Convert.ToInt32(Console.ReadLine ());
+Console.Write("Введём ординату точки (Y): ");
+int y = Convert.ToInt32(Console.ReadLine ());
+
+if ((x > 0) && (y > 0))
+    Console.Write("Точка в первой четверти\n");
+else if ((x < 0) && (y > 0))
+    Console.Write("Точка во второй четверти\n");
+else if ((x < 0) && (y < 0))
+    Console.Write("Точка в третьей четверти\n");
+else if ((x > 0) && (y < 0))
+    Console.Write("Точка в четвёртой четверти\n");
+else if ((x == 0) && (y == 0))
+    Console.Write("Точка в центре координат\n");
+else
+    Console.Write("Точка лежит на одной из осей\n");
