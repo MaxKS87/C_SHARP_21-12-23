54f8bc243a8497d743fb5b312a996e489bfd157b
Max <MaksKamarnitskiy@Gmail.com>
2023-12-26 23:42:24 +0300

    Les_2: HomeWork: Task_4

---
+++ Les_2/HomeWork/4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+
+Console.Write("Пожалуйста введите натуральное число: ");
+int element = Convert.ToInt32(Console.ReadLine ());
+// Можно ли использовать тип uint (подсмотрел его диапазон)?
+
+string set_of_num = "";
+if (element > 0)
+{
+    while (element > 0)
+    {
+        set_of_num = element % 10 + "," + set_of_num;
+        element = element / 10;
+    }
+    Console.Write(set_of_num + "\n");
+}
+else
+    Console.Write("Ну просили же ввести НАТУРАЛЬНОЕ число...\n");
